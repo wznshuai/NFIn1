51ae58e99fc337010c0e0a5ee32ee5875ddfd96a
rona <luona01@chinatopcredit.com>
2017-04-20 10:29:28 +0800

    改变部分字体颜色

--- libaray/src/main/java/com/wzn/libaray/utils/StringUtils.java
+++ libaray/src/main/java/com/wzn/libaray/utils/StringUtils.java
@@ -1,5 +1,9 @@
 package com.wzn.libaray.utils;
 
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,5 +22,20 @@ public class StringUtils {
         Pattern p = Pattern.compile(pattern);
         Matcher m = p.matcher(str);
         return m.matches();
+    }
+
+    /**
+     * 改变字体颜色
+     *
+     * @param str
+     * @param color 原字体大小的倍数
+     * @return
+     */
+    public static SpannableString strChangeColor(String str, int start, int end, int color) {
+        SpannableString span = new SpannableString(str);
+        span.setSpan(new ForegroundColorSpan(color), start,
+                end,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return span;
     }
 }
